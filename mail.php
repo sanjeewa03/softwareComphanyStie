e3194603b9290e4250891871340b17c1bf4aaaab
sanjeewa03 <sanjeewasenarathna03@gmail.com>
2019-08-25 19:50:58 +0530

    second

--- mail.php
+++ mail.php
@@ -1,27 +1,39 @@
 
 <?php
-require '/usr/share/php/libphp-phpmailer/class.phpmailer.php';
-require '/usr/share/php/libphp-phpmailer/class.smtp.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'C:\xampp\composer\vendor\autoload.php';
+require 
+
 $mail = new PHPMailer;
-$mail->setFrom('sanjeewwasenarathna03@gmail.com');
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$message2 = nl2br('Message from '.$name.' ('.$email.')\n
+'.$message);
+$mail->setFrom($email);
 $mail->addAddress('sanjeewasenarathna03@gmail.com');
 $mail->Subject = $_POST["subject"];
-$mail->Body = 'Hello! use PHPMailer to send email using PHP';
+$mail->Body = nl2br('Message from '.$name.' ('.$email.')<br><br><br>'.$message);
 $mail->IsSMTP();
 $mail->SMTPSecure = 'ssl';
 $mail->Host = 'ssl://smtp.gmail.com';
 $mail->SMTPAuth = true;
 $mail->Port = 465;
+$mail->IsHTML(true);
+
 
 //Set your existing gmail address as user name
 $mail->Username = 'sanjeewasenarathna03@gmail.com';
 
 //Set the password of your gmail address here
-$mail->Password = '**********';
+$mail->Password = '*****';
 if(!$mail->send()) {
   echo 'Email is not sent.';
   echo 'Email error: ' . $mail->ErrorInfo;
+  echo $message2;
 } else {
   echo 'Email has been sent.';
+  echo $message2;
 }
 ?>
